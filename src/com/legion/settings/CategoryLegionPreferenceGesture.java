8ac22401daef5cc0360b76a2ed3921998dba9550
AnonHacker47 <rubeenbilal47@gmail.com>
2020-09-01 19:41:05 +0000

    New Legion Settings Layout:
    -Intgrated Lottie Animations with Different Colours in Light/Dark themes
    -Redone Layout, courtesy of RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/legion/settings/CategoryLegionPreferenceGesture.java
+++ src/com/legion/settings/CategoryLegionPreferenceGesture.java
@@ -35,14 +35,14 @@ import android.widget.TextView;
 import com.android.settings.R;
 import com.android.settings.Utils;
 
-public class CategoryLegionPreference extends Preference {
+public class CategoryLegionPreferenceGesture extends Preference {
 
     private final View.OnClickListener mClickListener = v -> performClick(v);
 
     private boolean mAllowDividerAbove;
     private boolean mAllowDividerBelow;
 
-    public CategoryLegionPreference(Context context, AttributeSet attrs) {
+    public CategoryLegionPreferenceGesture(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
@@ -53,10 +53,10 @@ public class CategoryLegionPreference extends Preference {
                 R.styleable.Preference_allowDividerBelow, false);
         a.recycle();
 
-        setLayoutResource(R.layout.category_legion_preference);
+        setLayoutResource(R.layout.gesture_anim);
     }
 
-    public CategoryLegionPreference(Context context, View view) {
+    public CategoryLegionPreferenceGesture(Context context, View view) {
         super(context);
     }
 
