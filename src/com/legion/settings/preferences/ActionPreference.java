Project-Xtended/packages_apps_Xtensions

HW Keys customization support [2/2]
Based on DUI

jhenrique09 edits: make it DUI independent

Original commit message:

Same robust action library as used on software navigation. Supports
single tap, double tap, and long press. As a precautionary measure,
single tap back and single tap home are fixed and can not be changed.
Camera button actions are not supported at this time. We will bring
in wake key support at a later time.

Also includes:

DUI: Initial checkin for Oreo [4/7]

src/com/msm/xtended/preferences/ActionPreference.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.msm.xtended.preferences;
+
+import java.util.Map;
+
+import com.android.internal.util.hwkeys.ActionConstants.ConfigMap;
+import com.android.internal.util.hwkeys.ActionConstants.Defaults;
+import com.android.internal.util.hwkeys.ActionHolder;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+import com.android.internal.util.hwkeys.Config.ButtonConfig;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}
+
+

src/com/msm/xtended/preferences/CustomActionListAdapter.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.msm.xtended.preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}
+
+

ziasam/packages_apps_LegionSettings

src/com/legion/settings/preferences/ActionPreference.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.legion.settings.preferences;
+
+import java.util.Map;
+
+import com.android.internal.util.hwkeys.ActionConstants.ConfigMap;
+import com.android.internal.util.hwkeys.ActionConstants.Defaults;
+import com.android.internal.util.hwkeys.ActionHolder;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+import com.android.internal.util.hwkeys.Config.ButtonConfig;
+
+import android.content.Context;
+import androidx.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}
+
+

src/com/legion/settings/preferences/CustomActionListAdapter.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.legion.settings.preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}
+
+